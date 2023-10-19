c7f4db5dc0ee6ec22b7dda87a8dd45122947c89e
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-10-19 17:16:38 +0200

    added barebones spring app

---
+++ src/main/java/com/zegline/thubot/ThuBotApplication.java
@@ -0,0 +1,13 @@
+package com.zegline.thubot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThuBotApplication.class, args);
+	}
+
+}
