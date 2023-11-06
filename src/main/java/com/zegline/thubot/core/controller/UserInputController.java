8e6498cdf48250d5249526fa25e11d8783729d9e
osama <os.os20.osama@gmail.com>
2023-11-06 19:37:48 +0100

    Added openAiAPi class and modified UserInputController

--- src/main/java/com/zegline/thubot/core/controller/UserInputController.java
+++ src/main/java/com/zegline/thubot/core/controller/UserInputController.java
@@ -5,11 +5,6 @@
 * */
 package com.zegline.thubot.core.controller;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zegline.thubot.core.service.openai.OpenAIService;
 
 @RestController
