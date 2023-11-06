cfd854db8a97831d25403d3046da045f307bfed2
Eugen Zdrincu <eugen.zdrincu@deutschepost.de>
2023-11-06 10:44:27 +0100

    added git info to debug config

--- src/main/java/com/zegline/thubot/core/controller/GUIController.java
+++ src/main/java/com/zegline/thubot/core/controller/GUIController.java
@@ -1,13 +1,25 @@
 package com.zegline.thubot.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GUIController {
 
+    @Autowired
+    private InfoEndpoint infoEndpoint;
+
     @GetMapping("/")
-    public String getIndex() {
+    public String getIndex(Model model) {
+        // Fetch JSON data from /actuator/info
+        String jsonData = infoEndpoint.info().get("git").toString();
+
+        // Add the JSON data to the model
+        model.addAttribute("jsonData", jsonData);
+
         return "index";
     }
 
