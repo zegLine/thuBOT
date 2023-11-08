24cd08c573661a9a6cc5c289188ea9b2e2d8fc04
osama <os.os20.osama@gmail.com>
2023-11-08 16:30:05 +0100

    added get controller dialognode

--- src/main/java/com/zegline/thubot/core/controller/DialogNodeController.java
+++ src/main/java/com/zegline/thubot/core/controller/DialogNodeController.java
@@ -2,14 +2,18 @@ package com.zegline.thubot.core.controller;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.zegline.thubot.core.model.DialogNode;
 import com.zegline.thubot.core.repository.DialogNodeRepository;
@@ -31,14 +35,34 @@ public class DialogNodeController {
         return d;
     }
 
-    @GetMapping("/busisn")
-    public Set<DialogNode> getAll() {
-        Iterable<DialogNode> a = dnr.findAll();
-        Set<DialogNode> cute = new HashSet<>() ;
-        for (DialogNode dialogNode : a) {
-            cute.add(dialogNode);
+    @GetMapping("/get")
+    public Set<DialogNode> get(@RequestBody (required = false) Map<String, String> body) {
+        Set<DialogNode> returned = new HashSet<>() ;
+        Iterable<DialogNode> a;
+        if(body == null){
+            a = dnr.findAll();
+            
+            for (DialogNode dialogNode : a) {
+                returned.add(dialogNode);
+            }
+
+            return returned;
         }
 
-        return cute;
+        if(!body.get("id").isEmpty()) {
+            Optional<DialogNode> match = dnr.findById(body.get("id"));
+            if (!match.isPresent()) {
+                throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "couldn't find node"
+                );
+            }
+            returned.add(match.get());
+
+            return returned;
+        }
+
+        throw new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "id cannot be empty"
+        );
     }
 }
