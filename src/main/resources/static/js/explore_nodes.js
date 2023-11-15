d6d2c8c0bef44f13c970da7f5b96f312e3c1c498
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-11-15 20:15:28 +0100

    added barebone gui dialognode explore

---
+++ src/main/resources/static/js/explore_nodes.js
@@ -0,0 +1,42 @@
+function toggleChildren(nodeElement) {
+    const children = nodeElement.nextElementSibling;
+    if (children) {
+        children.classList.toggle('hidden');
+    }
+}
+
+function createNodeElement(entity) {
+    const nodeDiv = document.createElement('div');
+    nodeDiv.className = 'node';
+    nodeDiv.innerHTML = `<span class="dialog-text">${entity.dialogText}</span> - <span class="msg-text">${entity.msgText}</span>`;
+    nodeDiv.onclick = () => toggleChildren(nodeDiv);
+    return nodeDiv;
+}
+
+function createMap(entities, parentElement) {
+    entities.forEach(entity => {
+        const nodeElement = createNodeElement(entity);
+        parentElement.appendChild(nodeElement);
+
+        if (entity.children && entity.children.length) {
+            const childrenContainer = document.createElement('div');
+            childrenContainer.className = 'connection hidden';
+            parentElement.appendChild(childrenContainer);
+            createMap(entity.children, childrenContainer);
+        }
+    });
+}
+
+function fetchDataAndDisplayMap() {
+    fetch('http://localhost:8080/api/dialognode/get')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('map-container');
+            createMap(data, container);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+fetchDataAndDisplayMap();
