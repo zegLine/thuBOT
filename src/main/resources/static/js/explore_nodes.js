046cc057a70e5ae854ece503ffe2a9d22443f00e
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-11-15 21:45:49 +0100

    added basic gui prototype of prompts

--- src/main/resources/static/js/explore_nodes.js
+++ src/main/resources/static/js/explore_nodes.js
@@ -1,38 +1,59 @@
-function toggleChildren(nodeElement) {
-    const children = nodeElement.nextElementSibling;
-    if (children) {
-        children.classList.toggle('hidden');
+function hideSiblingsExcept(element) {
+    let sibling = element.parentElement.firstChild;
+    while (sibling) {
+        if (sibling !== element && sibling.classList.contains('node')) {
+            sibling.style.display = 'none';
+        }
+        sibling = sibling.nextSibling;
     }
+}
+
+function showChildren(nodeElement) {
+    const childrenContainer = nodeElement.nextElementSibling;
+    if (childrenContainer && childrenContainer.classList.contains('connection')) {
+        childrenContainer.style.display = 'block';
+    }
+}
+
+function nodeClickHandler(nodeElement) {
+    console.log('Node clicked:', nodeElement); // Log to check if the click is registered
+    hideSiblingsExcept(nodeElement);
+    showChildren(nodeElement);
 }
 
 function createNodeElement(entity) {
     const nodeDiv = document.createElement('div');
     nodeDiv.className = 'node';
     nodeDiv.innerHTML = `<span class="dialog-text">${entity.dialogText}</span> - <span class="msg-text">${entity.msgText}</span>`;
-    nodeDiv.onclick = () => toggleChildren(nodeDiv);
+    nodeDiv.onclick = () => nodeClickHandler(nodeDiv);
     return nodeDiv;
 }
 
-function createMap(entities, parentElement) {
+function createMap(entities, parentElement, isRoot = false) {
+    const container = document.createElement('div');
+    container.className = 'connection';
+    if (!isRoot) {
+        container.classList.add('hidden');
+    }
+    parentElement.appendChild(container);
+
     entities.forEach(entity => {
         const nodeElement = createNodeElement(entity);
-        parentElement.appendChild(nodeElement);
+        container.appendChild(nodeElement);
 
         if (entity.children && entity.children.length) {
-            const childrenContainer = document.createElement('div');
-            childrenContainer.className = 'connection hidden';
-            parentElement.appendChild(childrenContainer);
-            createMap(entity.children, childrenContainer);
+            createMap(entity.children, nodeElement);
         }
     });
 }
 
 function fetchDataAndDisplayMap() {
-    fetch('/api/dialognode/get')
+    fetch('http://localhost:8080/api/dialognode/get')
         .then(response => response.json())
         .then(data => {
+            console.log('Data received:', data); // Log to check if data is received correctly
             const container = document.getElementById('map-container');
-            createMap(data, container);
+            createMap(data, container, true);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
