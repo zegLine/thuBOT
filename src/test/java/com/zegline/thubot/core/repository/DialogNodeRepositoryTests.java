0db70c5261b55123254881895045ae041dbe10d7
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-12-06 21:16:28 +0100

    fixed application test file. The application test has been in the resources dir inside the TEST directory THIS WHOLE F TIME. I AM SO DONE, I SPENT 2 HOURS on this BECAUSE I DID NOT PAY ATTENTION

--- src/test/java/com/zegline/thubot/core/repository/DialogNodeRepositoryTests.java
+++ src/test/java/com/zegline/thubot/core/repository/DialogNodeRepositoryTests.java
@@ -1,26 +1,19 @@
 package com.zegline.thubot.core.repository;
 
-import com.zegline.thubot.ThuBotApplication;
 import com.zegline.thubot.core.model.DialogNode;
-import com.zegline.thubot.core.repository.DialogNodeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+
 
 import java.util.HashSet;
 
 @DataJpaTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class DialogNodeRepositoryTests {
 
     @Autowired
