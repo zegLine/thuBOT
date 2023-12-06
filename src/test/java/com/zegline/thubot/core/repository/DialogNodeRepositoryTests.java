c623b7935088f87c364ff0f4eb205db3bdc39717
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-12-06 20:58:49 +0100

    fixed tests datasource, now using the provided test configuration

--- src/test/java/com/zegline/thubot/core/repository/DialogNodeRepositoryTests.java
+++ src/test/java/com/zegline/thubot/core/repository/DialogNodeRepositoryTests.java
@@ -1,22 +1,26 @@
-package com.thubot.core.repository;
+package com.zegline.thubot.core.repository;
 
 import com.zegline.thubot.ThuBotApplication;
 import com.zegline.thubot.core.model.DialogNode;
 import com.zegline.thubot.core.repository.DialogNodeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
 
-
-@DataJpaTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-@ContextConfiguration(classes = ThuBotApplication.class)
+@DataJpaTest()
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DialogNodeRepositoryTests {
 
     @Autowired
