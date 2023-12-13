0e1d9f69f40e8356152d77589ab7c86a08c427aa
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-12-13 18:01:10 +0100

    fixed frontend api require login

--- src/main/java/com/zegline/thubot/core/config/SecurityConfig.java
+++ src/main/java/com/zegline/thubot/core/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
                 .authorizeRequests(authorize -> authorize
                         .requestMatchers("/database/**").hasRole("SYS")
                         .requestMatchers("/api/input/**").permitAll()
+                        .requestMatchers("/api/dialognode/get").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 )
