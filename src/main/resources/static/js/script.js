8e269fdb97ddb13eac675e51b71d38b847499afc
Firas <lutherpick@gmail.com>
2023-12-19 13:13:05 +0100

    Toggle button now resets the ChatBot(functions as intended). Added a Readmore button when the characters exceed a certain number. Fixed the issue where the ChatBot would still give the typing notification even after giving out an answer. More changes to be added.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,78 +2,98 @@ const chatbox = document.querySelector(".chatbox");
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const chatbotCloseBtn = document.querySelector(".close-btn");
 const messageInput = document.querySelector('.message-input');
-const typingIndicator = document.getElementById('typingIndicator');
+
+const maxMessageLength = 50;
 
 const createChatBubble = (text, isBot = true) => {
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", isBot ? "incoming" : "outgoing");
-    chatLi.innerHTML = isBot ? `<img src="https://i.imgur.com/0N92vb3.png" alt="Logo" class="chat-logo"><p>${text}</p>` : `<p>${text}</p>`;
+
+    // Create a paragraph element for text
+    const p = document.createElement("p");
+    p.innerText = text.length > maxMessageLength ? text.slice(0, maxMessageLength) + '...' : text;
+
+    // If the text is too long, add a "Read more" button
+    if (text.length > maxMessageLength) {
+        const readMoreBtn = document.createElement("button");
+        readMoreBtn.classList.add("read-more-btn");
+        readMoreBtn.innerText = "Read more";
+        // Function to toggle the message text
+        readMoreBtn.onclick = () => {
+            p.innerText = text;
+            readMoreBtn.remove();
+        };
+        p.appendChild(readMoreBtn);
+    }
+
+    // Append the paragraph to the chat list item
+    chatLi.appendChild(p);
+
+    if(isBot) {
+        const img = document.createElement("img");
+        img.src = "https://i.imgur.com/0N92vb3.png";
+        img.alt = "Logo";
+        img.classList.add("chat-logo");
+        chatLi.insertBefore(img, p);
+    }
+
     return chatLi;
 };
-const showTypingIndicator = () => {
-    // Create a new chat bubble as the typing indicator
-    const typingBubble = createChatBubble("THUBot is typing...", true);
-    typingBubble.id = 'typingIndicator'; // Assign the ID to the new element
-    chatbox.appendChild(typingBubble); // Add the typing indicator to the chatbox
-    setTimeout(() => {
-        typingBubble.remove(); // Remove the typing indicator from the DOM after 2 seconds
-    }, 4000);
+
+const clearChat = () => {
+    chatbox.innerHTML = ''; // Clears the chat history
+};
+const toggleText = (button, fullText) => {
+    const p = button.previousElementSibling; // The <p> element with the text
+    p.innerText = fullText;
+    button.remove(); // Remove the button after expanding the text
 };
 
 
-// Modified sendMessage function to send the user message to the server
-const sendMessage = async () => {
-    const message = messageInput.value.trim();
+const showTypingIndicator = () => {
+    const typingBubble = createChatBubble("THUBot is typing...", true);
+    chatbox.appendChild(typingBubble);
+    setTimeout(() => { typingBubble.remove(); }, 2000);
+};
+
+// Function to handle the sending of messages and receiving of responses
+// Function to handle the sending of messages and receiving of responses
+const sendMessage = async (message) => {
     if (message) {
-        // Display the user message in the chatbox
         chatbox.appendChild(createChatBubble(message, false));
-        messageInput.value = ''; // Clear the input after sending
-        chatbox.scrollTop = chatbox.scrollHeight; // Scroll to the bottom
+        messageInput.value = '';
+        chatbox.scrollTop = chatbox.scrollHeight;
+        showTypingIndicator(); // Show typing indicator
 
-        // Show typing indicator while waiting for response
-        showTypingIndicator();
-
-        // Send the message to the server for processing
         try {
-            const params = {
-                userInput: message
-            }
+            const params = { userInput: message };
             const response = await fetch('../api/input/ask?' + new URLSearchParams(params).toString(), {
                 method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
+                headers: { 'Content-Type': 'application/json' }
             });
             const responseData = await response.json();
+            chatbox.querySelector('.chat:last-child').remove(); // Remove typing indicator immediately after getting the response
 
-            // Hide typing indicator now that response has been received
-            typingIndicator.style.display = 'none';
-
-            // Process the response and display it in the chatbox
             if (responseData && responseData.msgText) {
                 chatbox.appendChild(createChatBubble(responseData.msgText));
-                if (responseData.children && responseData.children.length > 0) {
+                if (responseData.children) {
                     chatbox.appendChild(createResponseButtons(responseData.children));
                 }
             } else {
-                // Handle any case where responseData is not as expected
                 chatbox.appendChild(createChatBubble("Sorry, I didn't understand that."));
             }
         } catch (error) {
-            // Hide typing indicator if there's an error
-            typingIndicator.style.display = 'none';
             console.error('Error sending message:', error);
             chatbox.appendChild(createChatBubble("There was an error processing your message."));
         }
-
-        chatbox.scrollTop = chatbox.scrollHeight; // Scroll to the bottom
+        chatbox.scrollTop = chatbox.scrollHeight;
     }
 };
+
 
 const createResponseButtons = (children) => {
     const buttonsDiv = document.createElement("div");
     buttonsDiv.classList.add("response-buttons");
-
     children.forEach(child => {
         const button = document.createElement("button");
         button.classList.add("btn-response");
@@ -81,30 +101,25 @@ const createResponseButtons = (children) => {
         button.onclick = () => handleNodeSelection(child);
         buttonsDiv.appendChild(button);
     });
-
     return buttonsDiv;
 };
 
 const handleNodeSelection = (node) => {
-
-    // Show typing indicator
     showTypingIndicator();
-    // Use setTimeout to wait for typing indicator before showing the chat bubble
     setTimeout(() => {
-        // Append chat bubble after typing indicator has finished
         chatbox.appendChild(createChatBubble(node.msgText));
-        if (node.children && node.children.length > 0) {
+        if (node.children) {
             chatbox.appendChild(createResponseButtons(node.children));
         }
         chatbox.scrollTop = chatbox.scrollHeight;
-    }, 4000); // This should match the timeout used in showTypingIndicator
-}
+    }, 2000);
+};
+
 const fetchAndDisplayRootNodes = async () => {
     try {
         const response = await fetch('../api/dialognode/get');
         const data = await response.json();
         if (data && data.length > 0) {
-            // Automatically select the first root node
             handleNodeSelection(data[0]);
         }
     } catch (error) {
@@ -112,16 +127,24 @@ const fetchAndDisplayRootNodes = async () => {
     }
 };
 
-// Event listener for the Enter key in the message input
-
 messageInput.addEventListener('keypress', (event) => {
-    if (event.keyCode === 13) { // 13 is the Enter key
-        event.preventDefault(); // Prevent default to avoid any form submission
-        sendMessage(); // Call the function to send the message
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        sendMessage(messageInput.value.trim());
     }
 });
-chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+
+chatbotCloseBtn.addEventListener("click", () => {
+    clearChat();
+    document.body.classList.remove("show-chatbot");
+});
+
 chatbotToggler.addEventListener("click", () => {
+    const isChatbotOpen = document.body.classList.contains("show-chatbot");
     document.body.classList.toggle("show-chatbot");
-    fetchAndDisplayRootNodes();
+    if (isChatbotOpen) {
+        clearChat();
+    } else {
+        fetchAndDisplayRootNodes();
+    }
 });
