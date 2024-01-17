64f71b16294b3b29bf6dac10a519a7d0613d3333
Josh <oldridgejoshua@gmail.com>
2024-01-17 17:07:46 +0100

    changed settings to allow import and export in .js files

--- src/main/resources/.eslintrc.js
+++ src/main/resources/.eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
         }
     ],
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "rules": {
         "no-unused-vars": "warn",
